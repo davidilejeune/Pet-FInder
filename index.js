91b5c21804fc4291201d9d2c9fbf5403bff801af
davidilejeune <davidilejeune@aol.com>
2023-08-07 09:40:15 -0400

    Upload completed code

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// For GET - all pets, I need to make code that will be able to send the result 
+// For GET -pet by name, I need to set the req params and make code to send the result
+// For GET - pet by owner name, I need to set the req params and make code to send the result also
+
 // import the pets array from data.js
 const pets = require('./data');
 
@@ -10,7 +14,7 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile('index.html')
 });
 
 // hello world route
@@ -21,31 +25,31 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets);
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const { name } = req.params
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    res.send(pet);
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const { owner } = req.params
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.send(pet);
 });
 
 app.listen(PORT, () => {
